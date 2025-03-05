47a66b085d6ae55be999a33aee0a5c8560e98457
Aditi P <adip.2k5@gmail.com>
2025-03-05 12:22:49 +0530

    frontend

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,10 +1,9 @@
-import Chat from '../components/chat'
 import Navbar from '@/components/navbar'
 import HeroSection from '@/components/Hero'
 import AboutProject from '@/components/AboutProject'
 import AboutNITH from '@/components/AboutNIT'
 import AboutMamta from '@/components/AboutMA'
-
+import WasteClassifier from '@/components/Waste'
 export default function Home() {
   return (
     <main className="min-h-screen bg-white">
@@ -13,8 +12,7 @@ export default function Home() {
       <AboutProject />
       <AboutNITH />
       <AboutMamta />
-      <h1 className="text-center text-3xl font-bold mb-4 mt-8">Come Test Our Model</h1>
-      <Chat/>
+      <WasteClassifier/>
     </main>
   )
 }
